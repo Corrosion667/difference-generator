7205b5816b7bdc45f4a9b176e2b95dbc7adad394
Artem Nechai <lenikol95@mail.ru>
2021-08-15 21:27:52 +0300

    update gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 """This script shows basic info about the progam: its usage and syntax."""
+import argparse
+
+
+def main():
+    """Show info for gendiff program."""
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    args = parser.parse_args()
+    print(args)
+
+
+if __name__ == '__main__':
+    main()
