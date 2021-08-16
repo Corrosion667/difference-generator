10207da5800c26b855432c719bc882998fdc4d23
Artem Nechai <lenikol95@mail.ru>
2021-08-16 20:49:46 +0300

    update the gendiff script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -10,7 +10,7 @@ def main():
     parser.add_argument('second_file', type=str)
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    print(args)
+    print(generate_diff(args.first_file, args.second_file))
 
 
 if __name__ == '__main__':
