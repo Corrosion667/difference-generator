bf5997f824b0505dc4a31e91130450af0f03120c
Artem Nechai <lenikol95@mail.ru>
2021-08-17 23:10:43 +0300

    update test for json difference generator

--- tests/test_gendiff_engine.py
+++ tests/test_gendiff_engine.py
@@ -5,6 +5,8 @@ from tests.fixtures.estimated_json_result import estimated_difference_json
 
 
 def test_generate_diff_json():
-    assert generate_diff('tests/fixtures/first_test_file.json',
-                         'tests/fixtures/second_test_file.json',
-                         ) == estimated_difference_json
+    """Basic test for json defference generator."""
+    assert generate_diff(
+        'tests/fixtures/first_test_file.json',
+        'tests/fixtures/second_test_file.json',
+    ) == estimated_difference_json
