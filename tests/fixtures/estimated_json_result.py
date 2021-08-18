06a7c2f5ab2f807009b3be5b0a8793b61a4f8545
Artem Nechai <lenikol95@mail.ru>
2021-08-18 21:18:10 +0300

    update test scenario for json gendiff

--- tests/fixtures/estimated_json_result.py
+++ tests/fixtures/estimated_json_result.py
@@ -1,7 +1,9 @@
 """This module contains result fixtures for tests."""
 
 estimated_difference_json = (
-    '{\n - follow: false\n   host: hexlet.io\n'
+    '{\n   accept: true\n - boolean: true\n'
+    + ' + boolean: false\n - check: null\n'
+    + ' - follow: false\n   host: hexlet.io\n'
     + ' - proxy: 123.234.53.22\n - timeout: 50\n'
     + ' + timeout: 20\n + verbose: true\n}'
 )
