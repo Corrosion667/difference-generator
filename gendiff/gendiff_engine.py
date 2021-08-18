b08d5f11f0009d5c8068feb6833be2800a9771ab
Artem Nechai <lenikol95@mail.ru>
2021-08-18 21:09:20 +0300

    update gendiff engine

--- gendiff/gendiff_engine.py
+++ gendiff/gendiff_engine.py
@@ -2,6 +2,24 @@
 import json
 
 
+def converted(python_value):
+    """Convert Python's bools and None to required format.
+
+    Args:
+        python_value: input value to be examined and converted if necessary.
+
+    Returns:
+        Value or converted value if it is True, False or None
+    """
+    if python_value is True:
+        return 'true'
+    elif python_value is False:
+        return 'false'
+    elif python_value is None:
+        return 'null'
+    return python_value
+
+
 def generate_diff(file_path1, file_path2):
     """Get differences between two files.
 
@@ -19,11 +37,26 @@ def generate_diff(file_path1, file_path2):
     difference = ['{']
     keys = sorted(first_file.keys() | second_file.keys())
     for key in keys:
-        if first_file.get(key) == second_file.get(key):
-            difference.append('   {0}: {1}'.format(key, first_file.get(key)))
+        if key in (first_file.keys() - second_file.keys()):
+            difference.append(
+                ' - {0}: {1}'.format(key, converted(first_file.get(key))),
+            )
+        elif key in (second_file.keys() - first_file.keys()):
+            difference.append(
+                ' + {0}: {1}'.format(key, converted(second_file.get(key))),
+            )
         else:
-            difference.append(' - {0}: {1}'.format(key, first_file.get(key)))
-            difference.append(' + {0}: {1}'.format(key, second_file.get(key)))
+            if first_file.get(key) == second_file.get(key):
+                difference.append('   {0}: {1}'.format(
+                    key, converted(first_file.get(key)),
+                ),
+                )
+            else:
+                difference.append(
+                    ' - {0}: {1}'.format(key, converted(first_file.get(key))),
+                )
+                difference.append(
+                    ' + {0}: {1}'.format(key, converted(second_file.get(key))),
+                )
     difference.append('}')
-    difference = [_ for _ in difference if 'None' not in _]
     return '\n'.join(difference)
