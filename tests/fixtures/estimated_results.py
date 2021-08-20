4298b15fa0327950ca565ff99e6dfad506a612ee
Artem Nechai <lenikol95@mail.ru>
2021-08-20 22:13:52 +0300

    update tests

---
+++ tests/fixtures/estimated_results.py
@@ -0,0 +1,18 @@
+"""This module contains fixtures of results for tests."""
+
+estimated_diff_json = (
+    '{\n   accept: true\n - boolean: true\n'
+    + ' + boolean: false\n - check: null\n'
+    + ' - follow: false\n   host: hexlet.io\n'
+    + ' - proxy: 123.234.53.22\n - timeout: 50\n'
+    + ' + timeout: 20\n + verbose: true\n}'
+)
+
+
+estimated_diff_yaml = (
+    '{\n   accept: true\n - boolean: true\n'
+    + ' + boolean: false\n - check: null\n'
+    + ' - follow: false\n   host: hexlet.io\n'
+    + ' - proxy: 123.234.53.22\n - timeout: 50\n'
+    + ' + timeout: 20\n + verbose: true\n}'
+)
--- tests/test_gendiff_engine.py
+++ tests/test_gendiff_engine.py
@@ -1,8 +1,7 @@
 """This is a module to test gendiff program."""
 
+from fixtures.estimated_results import estimated_diff_json, estimated_diff_yaml
 from gendiff.gendiff_engine import generate_diff
-from tests.fixtures.estimated_json_result import estimated_difference_json
-from tests.fixtures.estimated_yaml_result import estimated_difference_yaml
 
 
 def test_generate_diff_json():
@@ -10,7 +9,7 @@ def test_generate_diff_json():
     assert generate_diff(
         'tests/fixtures/first_test_file.json',
         'tests/fixtures/second_test_file.json',
-    ) == estimated_difference_json
+    ) == estimated_diff_json
 
 
 def test_generate_diff_yml():
@@ -18,4 +17,4 @@ def test_generate_diff_yml():
     assert generate_diff(
         'tests/fixtures/first_test_file.yml',
         'tests/fixtures/second_test_file.yaml',
-    ) == estimated_difference_yaml
+    ) == estimated_diff_yaml
