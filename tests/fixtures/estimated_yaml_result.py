6b071f20dfea85fc30ee724fc5fd7a44b299db9a
Artem Nechai <lenikol95@mail.ru>
2021-08-20 22:09:01 +0300

    add tests for yaml format

---
+++ tests/fixtures/estimated_yaml_result.py
@@ -0,0 +1,9 @@
+"""This module contains result of yaml fixtures for tests."""
+
+estimated_difference_yaml = (
+    '{\n   accept: true\n - boolean: true\n'
+    + ' + boolean: false\n - check: null\n'
+    + ' - follow: false\n   host: hexlet.io\n'
+    + ' - proxy: 123.234.53.22\n - timeout: 50\n'
+    + ' + timeout: 20\n + verbose: true\n}'
+)
--- tests/test_gendiff_engine.py
+++ tests/test_gendiff_engine.py
@@ -2,11 +2,20 @@
 
 from gendiff.gendiff_engine import generate_diff
 from tests.fixtures.estimated_json_result import estimated_difference_json
+from tests.fixtures.estimated_yaml_result import estimated_difference_yaml
 
 
 def test_generate_diff_json():
-    """Basic test for json defference generator."""
+    """Basic test for json difference generator."""
     assert generate_diff(
         'tests/fixtures/first_test_file.json',
         'tests/fixtures/second_test_file.json',
     ) == estimated_difference_json
+
+
+def test_generate_diff_yml():
+    """Basic test for yaml difference generator."""
+    assert generate_diff(
+        'tests/fixtures/first_test_file.yml',
+        'tests/fixtures/second_test_file.yaml',
+    ) == estimated_difference_yaml
