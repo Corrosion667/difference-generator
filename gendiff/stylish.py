f01f7750cf52623f39074d202cf169db504db86e
Artem Nechai <lenikol95@mail.ru>
2021-08-21 21:19:40 +0300

    update stylish function

--- gendiff/stylish.py
+++ gendiff/stylish.py
@@ -4,4 +4,18 @@
 def stylished(diff):
     """Convert diff to a CLI notion.
 
+    Args:
+        diff: generated difference between two files.
+
+    Returns:
+        Difference formated into string with necessary syntax.
     """
+    difference = '{\n'
+    for each in diff:
+        if each[2] == 'both':
+            difference = difference + '   {0}: {1}\n'.format(each[0], each[1])
+        elif each[2] == 'first':
+            difference = difference + ' - {0}: {1}\n'.format(each[0], each[1])
+        else:
+            difference = difference + ' + {0}: {1}\n'.format(each[0], each[1])
+    return (difference + '}')
