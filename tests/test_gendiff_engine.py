1816b7ec6fd8e381d489188042dd2b3a3a6dbf00
Artem Nechai <lenikol95@mail.ru>
2021-08-21 21:20:13 +0300

    update tests for 2 phased mechanic of gendiff

--- tests/test_gendiff_engine.py
+++ tests/test_gendiff_engine.py
@@ -1,6 +1,7 @@
 """This is a module to test gendiff program."""
 import pytest
 from gendiff.gendiff_engine import generate_diff
+from gendiff.stylish import stylished
 from tests.fixtures.estimated_results import (
     estimated_diff_flat_json,
     estimated_diff_flat_yaml,
@@ -9,18 +10,18 @@ from tests.fixtures.estimated_results import (
 
 def test_generate_diff_flat_json():
     """Basic test for flat json difference generator."""
-    assert generate_diff(
+    assert stylished(generate_diff(
         'tests/fixtures/flat_test_file1.json',
         'tests/fixtures/flat_test_file2.json',
-    ) == estimated_diff_flat_json
+    )) == estimated_diff_flat_json
 
 
 def test_generate_diff_flat_yml():
     """Basic test for flat yaml difference generator."""
-    assert generate_diff(
+    assert stylished(generate_diff(
         'tests/fixtures/flat_test_file1.yml',
         'tests/fixtures/flat_test_file2.yaml',
-    ) == estimated_diff_flat_yaml
+    )) == estimated_diff_flat_yaml
 
 
 def test_generate_diff_wrong_formats():
