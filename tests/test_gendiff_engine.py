ce7f46dc5760febdc21bb4527d6b9495f93ed041
Artem Nechai <lenikol95@mail.ru>
2021-08-23 21:06:56 +0300

    change testing logic using with open test files

--- tests/test_gendiff_engine.py
+++ tests/test_gendiff_engine.py
@@ -2,32 +2,31 @@
 import pytest
 from gendiff.gendiff_engine import generate_diff
 from gendiff.stylish import stylished
-from tests.fixtures.estimated_results import (
-    estimated_diff_flat_json,
-    estimated_diff_flat_yaml,
-)
+
+with open('tests/fixtures/estimated_results/estimated_flat.txt') as result_file:
+    estimated_result_flat = result_file.read()
 
 
 def test_generate_diff_flat_json():
     """Basic test for flat json difference generator."""
     assert stylished(generate_diff(
-        'tests/fixtures/flat_test_file1.json',
-        'tests/fixtures/flat_test_file2.json',
-    )) == estimated_diff_flat_json
+        'tests/fixtures/test_files/flat_test_file1.json',
+        'tests/fixtures/test_files/flat_test_file2.json',
+    )) == estimated_result_flat
 
 
 def test_generate_diff_flat_yml():
     """Basic test for flat yaml difference generator."""
     assert stylished(generate_diff(
-        'tests/fixtures/flat_test_file1.yml',
-        'tests/fixtures/flat_test_file2.yaml',
-    )) == estimated_diff_flat_yaml
+        'tests/fixtures/test_files/flat_test_file1.yml',
+        'tests/fixtures/test_files/flat_test_file2.yaml',
+    )) == estimated_result_flat
 
 
 def test_generate_diff_wrong_formats():
     """Do not allow to parse unmatching formats."""
     with pytest.raises(ValueError):
         generate_diff(
-            'tests/fixtures/flat_test_file1.yml',
-            'tests/fixtures/flat_test_file2.json',
+            'tests/fixtures/test_files/flat_test_file1.yml',
+            'tests/fixtures/test_files/flat_test_file2.json',
         )
