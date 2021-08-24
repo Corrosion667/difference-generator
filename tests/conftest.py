12d22a0775ea621a732773f89817db1a4f870bee
Artem Nechai <lenikol95@mail.ru>
2021-08-24 21:48:45 +0300

    create tests for nested files

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,3 +12,15 @@ def get_flat_result():
     with open('tests/fixtures/estimated_results/flat.txt') as result_file:
         estimated_result_flat = result_file.read()
     return estimated_result_flat
+
+
+@pytest.fixture
+def get_nested_result():
+    """Get a fixture of estimated result for testing nested files.
+
+    Returns:
+        Estimated result for nested diff.
+    """
+    with open('tests/fixtures/estimated_results/nested.txt') as result_file:
+        estimated_result_nested = result_file.read()
+    return estimated_result_nested
--- tests/test_gendiff_engine.py
+++ tests/test_gendiff_engine.py
@@ -16,6 +16,18 @@ def test_generate_diff_flat_json(get_flat_result):
     )) == get_flat_result
 
 
+def test_generate_diff_nested_json(get_nested_result):
+    """Basic test for nested json difference generator.
+
+    Args:
+        get_nested_result: Estimated result for nested diff.
+    """
+    assert stylished(generate_diff(
+        'tests/fixtures/test_files/nested_test_file1.json',
+        'tests/fixtures/test_files/nested_test_file2.json',
+    )) == get_nested_result
+
+
 def test_generate_diff_flat_yml(get_flat_result):
     """Basic test for flat yaml difference generator.
 
@@ -26,6 +38,18 @@ def test_generate_diff_flat_yml(get_flat_result):
         'tests/fixtures/test_files/flat_test_file1.yml',
         'tests/fixtures/test_files/flat_test_file2.yaml',
     )) == get_flat_result
+
+
+def test_generate_diff_nested_yml(get_nested_result):
+    """Basic test for nested yaml difference generator.
+
+    Args:
+        get_nested_result: Estimated result for nested diff.
+    """
+    assert stylished(generate_diff(
+        'tests/fixtures/test_files/nested_test_file1.yml',
+        'tests/fixtures/test_files/nested_test_file2.yaml',
+    )) == get_nested_result
 
 
 def test_generate_diff_wrong_formats():
