9a3dcd0e450a86905c3044e2dbce6aa0871c622d
Artem Nechai <lenikol95@mail.ru>
2021-08-25 22:20:22 +0300

    update stylish formatter for nested files

--- gendiff/stylish.py
+++ gendiff/stylish.py
@@ -1,10 +1,45 @@
 """The module with CLI formatters for diffs."""
 
 
+def format_dict(element, level):
+    """Check wheteher an element of a diff is dict and format it.
+
+    Args:
+        element: the element of a diff to be checked.
+        level: current level of nesting.
+
+    Returns:
+        Formatted dict as a string.
+    """
+    level += 1
+
+    def walk(element, difference, level):
+        for key in sorted(element.keys()):
+            if isinstance(element[key], dict):
+                level += 1
+                difference = difference + \
+                    '{0}{1}: {2}\n'.format(
+                        ('    ' * level), key, walk(element[key], '{\n', level))
+                level -= 1
+            else:
+                level += 1
+                difference = difference + \
+                    '{0}{1}: {2}\n'.format(
+                        ('    ' * level), key, element[key])
+                level -= 1
+        difference = difference + '{0}}}'.format(('    ' * level))
+        return difference
+    if isinstance(element, dict):
+        return walk(element, '{\n', level)
+    return element
+
+
 def stylished(diff):
     """Convert diff to a CLI notion.
+
     Args:
         diff: generated difference between two files.
+
     Returns:
         Difference formated into string with necessary syntax.
     """
@@ -12,32 +47,46 @@ def stylished(diff):
         for each in sequence:
             if isinstance(each[1], tuple):
                 difference = difference + \
-                    '{0}    {1}: '.format(('    '*level), each[0])
+                    '{0}    {1}: '.format(('    ' * level), each[0])
                 level += 1
                 difference = difference + \
                     '{0}\n'.format(walk(each[1], '{\n', level))
                 level -= 1
             elif isinstance(each[2], tuple):
                 difference = difference + \
-                    '{0}    {1}: '.format(('    '*level), each[0])
+                    '{0}    {1}: '.format(('    ' * level), each[0])
                 level += 1
                 difference = difference + \
                     '{0}\n'.format(walk(each[2], '{\n', level))
                 level -= 1
             elif each[1] == each[2]:
                 difference = difference + \
-                    '{0}    {1}: {2}\n'.format(('    '*level), each[0], each[1])
+                    '{0}    {1}: {2}\n'.format(
+                        ('    ' * level), each[0], format_dict(each[1], level),
+                    )
             elif each[2] is None:
                 difference = difference + \
-                    '{0}  - {1}: {2}\n'.format(('    '*level), each[0], each[1])
+                    '{0}  - {1}: {2}\n'.format(
+                        ('    ' * level),
+                        each[0], format_dict(each[1], level),
+                    )
             elif each[1] is None:
                 difference = difference + \
-                    '{0}  + {1}: {2}\n'.format(('    '*level), each[0], each[2])
+                    '{0}  + {1}: {2}\n'.format(
+                        ('    ' * level),
+                        each[0], format_dict(each[2], level),
+                    )
             else:
                 difference = difference + \
-                    '{0}  - {1}: {2}\n'.format(('    '*level), each[0], each[1])
+                    '{0}  - {1}: {2}\n'.format(
+                        ('    ' * level),
+                        each[0], format_dict(each[1], level),
+                    )
                 difference = difference + \
-                    '{0}  + {1}: {2}\n'.format(('    '*level), each[0], each[2])
-        difference = difference + '{0}}}'.format(('    '*level))
+                    '{0}  + {1}: {2}\n'.format(
+                        ('    ' * level),
+                        each[0], format_dict(each[2], level),
+                    )
+        difference = difference + '{0}}}'.format(('    ' * level))
         return (difference)
     return walk(diff, '{\n', 0)
