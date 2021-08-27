7e46e50ba0cc65034bbeda104a35c9083b591c00
Artem Nechai <lenikol95@mail.ru>
2021-08-27 18:24:52 +0300

    update main script with a default formatter

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,15 +5,31 @@ import argparse
 from gendiff.gendiff_engine import generate_diff
 from gendiff.stylish import stylished
 
+formatter_map = {'stylished': stylished}
+
 
 def main():
     """Execute the diff generator."""
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
-    parser.add_argument('-f', '--format', help='set format of output')
+    parser.add_argument(
+        '-f',
+        '--format',
+        type=str,
+        choices=formatter_map.keys(),
+        default='stylished',
+        help="""set format of output; default: stylished
+         (json-like format with - for deleted elements and + for added)
+         """,
+    )
     args = parser.parse_args()
-    print(stylished((generate_diff(args.first_file, args.second_file))))
+    print(
+        generate_diff
+        (
+            args.first_file, args.second_file, formatter_map[args.format],
+        ),
+    )
 
 
 if __name__ == '__main__':
