83a83a2457832c5a23721a5218cbd9fe27149857
Artem Nechai <lenikol95@mail.ru>
2021-08-28 23:46:45 +0300

    update plain formatter

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,2 +1,13 @@
+"""Thesis-like format with satements about adding, updating and removal."""
+
+
 def plained(diff):
+    """Convert diff to a CLI notion.
+
+    Args:
+        diff: generated difference between two files.
+
+    Returns:
+        Difference formated into string with necessary syntax.
+    """
     return diff
