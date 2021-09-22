8058ea57397c13e6e4ca2954a726b3a3c4f759fa
Artem Nechai <lenikol95@mail.ru>
2021-09-22 20:58:28 +0300

    update test for unsupported formats of file

--- tests/test_error.py
+++ tests/test_error.py
@@ -7,6 +7,6 @@ def test_generate_diff_wrong_formats():
     """Do not allow to parse unmatching formats."""
     with pytest.raises(ValueError):
         generate_diff(
-            'tests/fixtures/test_files/flat_test_file1.yml',
+            'tests/fixtures/test_files/unsupported_format.txt',
             'tests/fixtures/test_files/flat_test_file2.json',
         )
