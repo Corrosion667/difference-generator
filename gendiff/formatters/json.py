c4eb71893b28bf7d7ed58067367bd9bec7efb5eb
Artem Nechai <lenikol95@mail.ru>
2021-09-23 22:19:55 +0300

    update json formatter (sorting)

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -1,6 +1,15 @@
 """Classic json: list of lists with [key, value_file1, value_file2]."""
 import json
 
+def sort(diff):
+    diff.sort()
+    for each in diff:
+        if isinstance(each[1], list):
+            sort(each[1])
+        elif isinstance(each[2], list):
+            sort(each[2])
+    return diff
+
 
 def jsoned(diff):
     """Convert diff to a CLI notion.
@@ -11,4 +20,5 @@ def jsoned(diff):
     Returns:
         Difference in classic json format.
     """
-    return json.dumps(diff)
+    
+    return json.dumps(sort(diff))
